2af8d179972a23d94a7bd242467b5437acfb374c
saulo s2costa <saulotarsobc@gmial.com>
2020-08-16 15:59:06 -0300

    ok

--- class/Usuario.php
+++ class/Usuario.php
@@ -38,7 +38,7 @@ class Usuario {
     public function setDtcadastro($value){
         $this->dtcadastro = $value;
     }
-
+    //select por ID
     public function loadById($id){
 
         $sql = new Sql();
@@ -58,6 +58,50 @@ class Usuario {
 
         }
 
+    }
+    // listar
+    public static function getList(){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+    }
+    //buscar usuario
+    public static function search($login){
+
+        $sql = New Sql();
+
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=>"%".$login."%"
+        ));
+
+    }
+    //metodo para login
+    public function login($login, $password){
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+        ));
+
+        if (count($results) > 0){
+
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+        } else {
+
+            throw new Exception("Login e/ou senha inválidos!");
+
+        }
+        
     }
 
     public function __toString(){
--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//carrega um usuario
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
 
-$root->loadById(1);
+//carrega uma lista
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//carrega uma lista de usuarios buscando pelo login
+/* $search = Usuario::search("us");
+echo json_encode($search); */
+
+//carrea um usuario usando o login e senha
+$usuario = new Usuario();
+$usuario->login("Amauri", "cortina07");
+
+echo $usuario;
 
 ?>
